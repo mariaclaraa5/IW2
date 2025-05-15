f53bd1636f0774199b0fb6216952c745b5b1e068
mariaclaraa5 <mariaclaranunes202012@gmail.com>
2025-05-14 23:44:04 -0300

    script.js

--- listaiw2/js/script.js
+++ listaiw2/js/script.js
@@ -1,24 +1,25 @@
-
+//é responsável por colocar na outra div
 function Adicionar() {
   const checkboxes = document.querySelectorAll("#Div1 .checkbox");
   const destino = document.getElementById("Div2");
   checkboxes.forEach(checkbox => {
     if (checkbox.checked) {
-      const label = checkbox.parentElement; // mover o label junto
+      const label = checkbox.parentElement; 
       destino.appendChild(label);
-      checkbox.checked = false; // desmarca após mover
+      checkbox.checked = false; 
     }
 
 
      
   });
 }
+//retira os itens e manda de volta para a div anterior
 function Remover() {
   const destino = document.getElementById("Div2");
   const origem = document.getElementById("Div1");
   const checkboxes = document.querySelectorAll("#Div2 .checkbox");
   checkboxes.forEach(checkbox => {
-    const label = checkbox.parentElement; // mover o label junto
+    const label = checkbox.parentElement;
     origem.appendChild(label);
     checkbox.checked = false; // opcional: desmarca após mover
 
